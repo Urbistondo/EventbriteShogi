ceb8ce7fefdbd8885dccb139dc324c8d1f182786
Urbistondo <urbistondo.javier@gmail.com>
2020-02-29 17:55:52 +0100

    Adds Piece abstract class

---
+++ Pieces/Piece.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+class Color(Enum):
+    WHITE = 0
+    BLACK = 1
+
+
+class Piece:
+    description = None
+    color = False
+    representation = None
+    symbol = None
+
+    SYMBOL_BLACK = '^'
+    SYMBOL_WHITE = 'v'
+
+    def __init__(self, color=Color.WHITE):
+        self.color = color
+        self.symbol = self.SYMBOL_WHITE if color == Color.WHITE else self.SYMBOL_BLACK
+
+    def create_white(self):
+        return self.__init__(Color.WHITE)
+
+    def create_black(self):
+        return self.__init__(Color.BLACK)
+
+    def get_color(self):
+        return self.color
+
+    def to_string(self):
+        return self.representation + self.symbol
