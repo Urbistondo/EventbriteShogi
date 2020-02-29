e0c52899943d07d3d1e79d721495af92c1e1b1b0
Urbistondo <urbistondo.javier@gmail.com>
2020-02-29 18:38:17 +0100

    Adds board initialization logic

--- Board.py
+++ Board.py
@@ -1,3 +1,4 @@
+from Pieces.Piece import Color
 from Square import Square
 
 
@@ -13,6 +14,41 @@ class Board:
         self.rows = rows
         self.columns = columns
         self.grid = [[Square() for i in range(columns)] for j in range(rows)]
+
+    def initialize_board(self, piece_factory):
+        self.create_pieces(piece_factory, Color.WHITE)
+        self.create_pieces(piece_factory, Color.BLACK)
+
+    def create_pieces(self, piece_factory, color):
+        first_row = 0
+        second_row = 1
+        third_row = 2
+
+        if color == Color.BLACK:
+            first_row = -1
+            second_row = -2
+            third_row = -3
+
+        for i in range(self.columns):
+            self.grid[third_row][i].set_piece(piece_factory.create_pawn(color))
+
+        self.grid[second_row][1].set_piece(piece_factory.create_bishop(color))
+
+        self.grid[second_row][-2].set_piece(piece_factory.create_rook(color))
+
+        self.grid[first_row][0].set_piece(piece_factory.create_lance(color))
+        self.grid[first_row][-1].set_piece(piece_factory.create_lance(color))
+
+        self.grid[first_row][1].set_piece(piece_factory.create_knight(color))
+        self.grid[first_row][-2].set_piece(piece_factory.create_knight(color))
+
+        self.grid[first_row][2].set_piece(piece_factory.create_silver_general(color))
+        self.grid[first_row][-3].set_piece(piece_factory.create_silver_general(color))
+
+        self.grid[first_row][3].set_piece(piece_factory.create_gold_general(color))
+        self.grid[first_row][-4].set_piece(piece_factory.create_gold_general(color))
+
+        self.grid[first_row][4].set_piece(piece_factory.create_king(color))
 
     def to_string(self):
         print(self.rows)
