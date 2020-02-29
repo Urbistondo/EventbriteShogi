e41d8dcdbf4eef53851cdc2d94af026ec2d01c82
Urbistondo <urbistondo.javier@gmail.com>
2020-02-29 18:27:32 +0100

    Adds game  start/finish and turn changing methods

--- Game.py
+++ Game.py
@@ -21,3 +21,28 @@ class Game:
     def __init__(self, board, piece_factory):
         self.board = board
         self.piece_factory = piece_factory
+
+    def start_game(self):
+        self.start_date = datetime.datetime.now()
+        self.turn = 1
+        self.current_player = Color.WHITE
+
+        self.captured_white = []
+        self.captured_black = []
+
+    def finish_game(self):
+        self.end_date = datetime.datetime.now()
+        self.finished = True
+
+    def is_finished(self):
+        return self.finished
+
+    def get_turn(self):
+        return self.turn
+
+    def get_current_player(self):
+        return self.current_player
+
+    def next_turn(self):
+        self.turn += 1
+        self.current_player = Color.WHITE if self.current_player == Color.BLACK else Color.BLACK
