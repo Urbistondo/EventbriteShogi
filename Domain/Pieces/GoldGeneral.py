e97a2de2bfeec3c50489fffbca052eba327f5e9c
Urbistondo <urbistondo.javier@gmail.com>
2020-03-02 09:38:45 +0100

    Implements Gold General and Silver General movement logic

--- Domain/Pieces/GoldGeneral.py
+++ Domain/Pieces/GoldGeneral.py
@@ -1,4 +1,4 @@
-from Domain.Pieces.Piece import Piece
+from Domain.Pieces.Piece import Piece, Color
 
 
 class GoldGeneral(Piece):
@@ -6,3 +6,17 @@ class GoldGeneral(Piece):
         Piece.__init__(self, color)
         self.description = 'Gold General'
         self.representation = 'G'
+
+    def can_reach(self, origin_row, origin_col, destination_row, destination_col, color=None):
+        if color == Color.WHITE:
+            if abs(origin_row - destination_row) > 1 or abs(destination_row - destination_col) > 1 or (
+                    destination_row < origin_row and destination_col != origin_col):
+                return False
+            else:
+                return True
+        else:
+            if abs(origin_row - destination_row) > 1 or abs(destination_row - destination_col) > 1 or (
+                    destination_row > origin_row and destination_col != origin_col):
+                return False
+            else:
+                return True
--- Domain/Pieces/SilverGeneral.py
+++ Domain/Pieces/SilverGeneral.py
@@ -1,4 +1,4 @@
-from Domain.Pieces.Piece import Piece
+from Domain.Pieces.Piece import Piece, Color
 
 
 class SilverGeneral(Piece):
@@ -6,3 +6,19 @@ class SilverGeneral(Piece):
         Piece.__init__(self, color)
         self.description = 'Silver General'
         self.representation = 'S'
+
+    def can_reach(self, origin_row, origin_col, destination_row, destination_col, color=None):
+        if color == Color.WHITE:
+            if abs(origin_row - destination_row) > 1 or abs(origin_col - destination_col) > 1 or (
+                    destination_row < origin_row and destination_col == origin_col) or (
+                    destination_row == origin_row and destination_col != origin_col):
+                return False
+            else:
+                return True
+        else:
+            if abs(origin_row - destination_row) > 1 or abs(origin_col - destination_col) > 1 or (
+                    destination_row > origin_row and destination_col == origin_col) or (
+                    destination_row == origin_row and destination_col != origin_col):
+                return False
+            else:
+                return True
